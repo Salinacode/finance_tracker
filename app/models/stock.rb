7c37e33cb5b38d61ce17a13dc08958e72d7f6b33
Salinacode <salinasubudhi103@gmail.com>
2023-02-23 12:44:46 +0530

    JavaScript responses to invalid search results

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,11 @@
 class Stock < ApplicationRecord
 	def self.new_lookup(ticker_symbol)
 	    client = IEX::Api::Client.new(publishable_token: publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],secret_token: 'sk_f0ecad403440405a91067cfe29543358',endpoint: 'https://sandbox.iexapis.com/v1')
-	    client.price(ticker_symbol)
+	    
+	    begin
+	    	new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+	    rescue => exception
+	    	return nil
+	    end
 	end
 end
